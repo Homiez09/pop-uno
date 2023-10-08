ad0eb38133b7de973093f807b0b15e5683804259
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2023-10-08 15:14:27 +0700

    [+update] fixed bug pps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 	
 	let main: HTMLElement;
 	let clicked: boolean = false;
+	let ppsShow: number = 0;
 	let pps: number = 0;
 
 	const audio = new Howl({
@@ -17,12 +18,17 @@
     });
 
 	const incrementCount = () => {
-		if (clicked) return;
+		pps++;
 		audio.play();
 		count.update((n) => n + 1);
 		tempCount.update((n) => n + 1);
-		clicked = true;
 	};
+
+	const incrementKeyPressCount = () => {
+		if (clicked) return;
+		clicked = true;
+		incrementCount();
+	}
 
 	const resetClicked = () => {
 		clicked = false;
@@ -36,27 +42,26 @@
 	};
 
 	const updateCountRecord = () => {
+		getTotalCount();
 		setInterval(async () => {
-			getTotalCount() 
-
 			if (!$tempCount) return;
 			await pb.collection('records').update<DataInterface>('r68rzn7phg2oplq', {
 				"name": "uno",
 				"count+" : $tempCount
-			}).then(
-				() => {
-					tempCount.set(0);
-				}
-			)
-
+			}).then((data) => {
+				$tempCount = 0;
+			})
+			
 		}, 1000);
 	};
 
 	const getTotalCount = async () => {
 		let record = await pb.collection('records').getOne("r68rzn7phg2oplq");
-			pps = record.count - parseInt($totalCount);
-			let countTotal = record.count;
-			totalCount.set(countTotal.toString());
+		pps = (record.count - parseInt($totalCount) > 0) ? record.count - parseInt($totalCount) : 0;
+		ppsShow = pps;
+		pps = 0;
+		let countTotal = record.count;
+		totalCount.set(countTotal.toString());
 	};
 
 	const abbreviateNumber = (pps: number) => {
@@ -69,22 +74,29 @@
 		return scaled.toFixed(1) + suffix;
 	}
 
+	const ppsShowUpdate = () => {
+		setInterval(async() => {
+			ppsShow = pps;
+			pps = 0;
+		}, 1000);
+	}
+
 	onMount(async () => {
 		updateCountRecord();
+		ppsShowUpdate();
 		await pb.autoCancellation(false);
-	
 	});
 
 </script>
 
-<svelte:body on:keydown={incrementCount} on:keyup={resetClicked}/>
+<svelte:body on:keydown={incrementKeyPressCount} on:keyup={resetClicked}/>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <main
 	bind:this={main}
 	class="no-select w-full h-screen flex flex-col items-center bg-transparent"
 	on:mousedown={incrementCount}
-	on:mouseup={resetClicked}
+	
 >
 	<img src="/uno.jpg" alt="uno" class="block fixed object-cover w-full h-full -z-10 bg-black" />
 	<img src="/title.png" alt="uno" class="mt-8 sm:w-8/12 md:w-2/4 lg:w-1/4" />
@@ -98,15 +110,10 @@
 	<p class="noselect text-3xl border-black text-white mt-8 bg-black rounded p-2">
 		Total: {(parseInt($totalCount) + $tempCount).toLocaleString()}
 		<span class="text-xs ml-1 text-green-400">
-			{pps !== undefined ? `+${abbreviateNumber(pps)} PPS` : '...'}
+			{`+ ${abbreviateNumber(ppsShow)} PPS`}
 		</span>
 	</p>
 </main>
-
-<!-- @firebase/firestore: Firestore (10.1.0): Could not reach Cloud Firestore backend. Connection failed 1 times. Most recent error: FirebaseError: [code=permission-denied]: Permission denied: Consumer 'project:undefined' has been suspended.
-This typically indicates that your device does not have a healthy Internet connection at the moment. The client will operate in offline mode until it is able to successfully connect to the backend. -->
-
-<!-- +page.svelte:53 Uncaught (in promise) FirebaseError: Failed to get document because the client is offline. -->
 
 <style>
 	.no-select {
